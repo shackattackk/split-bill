716618850652187a8fdb489e24c4788ce7740818
shackattackk <gaby_yusay2001@yahoo.com>
2025-05-29 14:19:06 -0400

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Split Party",
   description: "Split Bills, Not Friendships.",
+  icons: {
+    icon: '/favicon.png',
+  },
 };
 
 export default function RootLayout({
